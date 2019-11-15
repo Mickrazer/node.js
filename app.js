1ae95abed24ff6994ff9f613c1f117cb509bd06a
Mikhail Evdokimov <ekb.mik@yandex.ru>
2019-11-15 15:18:53 +0500

    change 2.0

--- app.js
+++ app.js
@@ -4,18 +4,13 @@ const path = require('path');
 const usersRouter = require(path.join(__dirname, 'routers/users.js'));
 const cardsRouter = require(path.join(__dirname, 'routers/cards.js'));
 const notFoundRouter = require(path.join(__dirname, 'routers/notFound.js'));
-
-
 const { PORT = 3000 } = process.env;
 
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use('/users', usersRouter);
 app.use('/cards', cardsRouter);
-app.use('*', usersRouter);
-
-
-
+app.use('*', notFoundRouter);
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`)
-})
+});
--- routers/users.js
+++ routers/users.js
@@ -1,22 +1,23 @@
 const path = require('path');
 const router = require('express').Router();
 const fsPromises = require('fs').promises;
-const users = fsPromises.readFile(path.join(__dirname, '../data/users.json'), {encoding: 'utf8'})
+
+const users = fsPromises.readFile(path.join(__dirname, '../data/user.json'), { encoding: 'utf8' })
   .catch((err) => {
     console.log(err);
   })
 
 router.get('/', (req, res) => {
-  users.then((arrUsers) => {
+  users.then((data) => {
     res.set('Content-Type', 'application/json');
-    res.send(arrUsers)
+    res.send(data);
   });
 });
 
 router.get('/:id', (req, res) => {
-    users.then((arrUsers) => {
+    users.then((data) => {
     res.set('Content-Type', 'application/json');
-    const user = JSON.parse(arrUsers).find((item) => item._id === req.params.id);
+    const user = JSON.parse(data).find((item) => item._id === req.params.id);
     if(user){
       res.send(user);
     } else {
