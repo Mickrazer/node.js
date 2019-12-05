3cae4d9168d7c5a6072ba359c95f5a3026d8afce
Mikhail Evdokimov <ekb.mik@yandex.ru>
2019-12-05 19:02:14 +0500

    after fix

--- app.js
+++ app.js
@@ -2,10 +2,9 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
 const usersRouter = require(path.join(__dirname, 'routers/users.js'));
-//const cardsRouter = require(path.join(__dirname, 'routers/cards.js'));
-const notFoundRouter = require(path.join(__dirname, 'routers/notFound.js'));
+const cardsRouter = require(path.join(__dirname, 'routers/cards.js'));
+const { error, someNotFound } = require(path.join(__dirname, 'routers/notFound.js'));
 const { PORT = 3000 } = process.env;
 
 mongoose.connect('mongodb://localhost:27017/mestodb', {
@@ -24,8 +23,9 @@ app.use((req, res, next) => {
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/users', usersRouter);
-//app.use('/cards', cardsRouter);
-app.use('*', notFoundRouter);
+app.use('/cards', cardsRouter);
+app.use('*', error);
+app.use('*', someNotFound);
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`)
