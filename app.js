1b37007688f3ffdd8e6e8703207504f75e105f0b
Mikhail Evdokimov <ekb.mik@yandex.ru>
2019-12-11 16:11:51 +0500

    sprint14

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const usersRouter = require(path.join(__dirname, 'routers/users.js'));
 const cardsRouter = require(path.join(__dirname, 'routers/cards.js'));
 const { error, someNotFound } = require(path.join(__dirname, 'routers/notFound.js'));
 const { PORT = 3000 } = process.env;
+const { createUser, login } = require('./controllers/users');
 
 mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
@@ -13,17 +14,12 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
   useFindAndModify: false,
 });
 
-app.use((req, res, next) => {
-  req.user = {
-    _id: '5de511589242026f5f417885',
-  };
-  next();
-})
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/users', usersRouter);
 app.use('/cards', cardsRouter);
+app.post('/signin', login);
+app.post('/signup', createUser);
 app.use('*', error);
 app.use('*', someNotFound);
 
--- routers/users.js
+++ routers/users.js
@@ -1,10 +1,10 @@
 const router = require('express').Router();
 const {
-  getUsers, getUser, addUser,
+  getUsers, getUser,
 } = require('../controllers/users');
 
 router.get('/', getUsers);
 router.get('/:userId', getUser);
-router.post('/', addUser);
+
 
 module.exports = router;
