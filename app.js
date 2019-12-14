f686200345f7d93ad9e3fb6df8189ba13f98c76a
Mikhail Evdokimov <ekb.mik@yandex.ru>
2019-12-14 17:24:43 +0500

    after fix 2.0

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const cardsRouter = require(path.join(__dirname, 'routers/cards.js'));
 const { error, someNotFound } = require(path.join(__dirname, 'routers/notFound.js'));
 const { PORT = 3000 } = process.env;
 const { createUser, login } = require('./controllers/users');
+const auth = require('middleweres/auth')
 
 mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
@@ -16,8 +17,8 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/users', usersRouter);
-app.use('/cards', cardsRouter);
+app.use('/users', auth, usersRouter);
+app.use('/cards', auth, cardsRouter);
 app.post('/signin', login);
 app.post('/signup', createUser);
 app.use('*', error);
