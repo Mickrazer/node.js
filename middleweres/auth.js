594c19d4b5eb48231a806507d687d8bd21dc23a8
Mikhail Evdokimov <ekb.mik@yandex.ru>
2019-12-23 23:57:51 +0500

    fix 2.0

--- middleweres/auth.js
+++ middleweres/auth.js
@@ -1,4 +1,5 @@
-require('dotenv').config();
+const path = require('path')
+require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
 const jwt = require('jsonwebtoken');
 const { ErrorUnauthorized } = require('../modul/errors');
 const { NODE_ENV, JWT_SECRET } = process.env;
