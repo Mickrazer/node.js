a64e060fa6534341d0338f31700d544541f2cbab
Mikhail Evdokimov <ekb.mik@yandex.ru>
2019-12-24 00:48:37 +0500

    fix 2.1

--- middleweres/auth.js
+++ middleweres/auth.js
@@ -5,7 +5,7 @@ const { ErrorUnauthorized } = require('../modul/errors');
 const { NODE_ENV, JWT_SECRET } = process.env;
 const key = require('../modul/key');
 
-console.log( {NODE_ENV, JWT_SECRET });
+
 module.exports = (req, res, next) => {
   const token = req.cookies.jwt;
   let payload;
